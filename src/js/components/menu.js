9d5c420ddb863a5be5406c6494d6ca75a62f0805
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2018-11-02 04:30:11 +0200

    1.01.001w

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -3,3 +3,12 @@ $(function(){
         $(this).toggleClass('open');
     });
 });
+
+//Fix menu position
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 1) {
+        $('#menu').addClass('menu_fixed');
+    } else {
+        $('#menu').removeClass('menu_fixed');
+    }
+});
