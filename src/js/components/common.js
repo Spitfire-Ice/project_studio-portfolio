974a11ae0f5dfdadafa7e8fe6bf6436579819393
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2018-11-07 02:15:24 +0200

    +gallery section +favicon +fix width header content container

--- src/js/components/common.js
+++ src/js/components/common.js
@@ -11,4 +11,17 @@ $('.slick-slider').slick({
     nextArrow: $('#slider_arrow_right')
   });
 
-  
+  //Gallery filters
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){
+        selectedClass = $(this).attr("data-rel");
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+        }, 300);
+
+    });
+});
